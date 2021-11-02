72d86ef00c4f91b9fd4be80b43409b45b0412935
stevekim72 <steve.kim72@yahoo.com>
2021-11-02 13:38:41 -0700

    Contentful Home page Hero Section

--- src/components/home-page/hero-en.js
+++ src/components/home-page/hero-en.js
@@ -1,31 +1,52 @@
 import * as React from 'react';
 import { Row, Col } from 'react-bootstrap/';
+import { StaticQuery, graphql } from 'gatsby';
 
 import HeroSVG from '../../svg/home-hero.svg';
 import PhoneSVG from '../../svg/phone.svg';
 
 const HeroSection = () => (
-  <section className={`section-hero`}>
-    <Row>
-      <Col lg={4} className={`order-last order-lg-first`}>
-        <div className={`hero-copy`}>
-          <h1 className={`hero-title`}>English Book Ladder</h1>
-          <p className={`hero-description`}>
-            Step into our interactive library where students walk out as
-            lifelong readers.
-          </p>
-          <div>
-            <a href='tel:03180225010' className={`cta-btn`}>
-              Call Us <PhoneSVG className={`cta-phone-icon`} />
-            </a>
-          </div>
-        </div>
-      </Col>
-      <Col lg={8} className={`order-first order-lg-last`}>
-        <HeroSVG className={`hero-svg-width`} />
-      </Col>
-    </Row>
-  </section>
+  <StaticQuery
+    query={graphql`
+      query HomePageHeroEnglish {
+        allContentfulHomePageAssembly {
+          nodes {
+            heroTitleEnglish
+            heroDescriptionEnglish {
+              heroDescriptionEnglish
+            }
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <section className={`section-hero`}>
+        <Row>
+          <Col lg={4} className={`order-last order-lg-first`}>
+            <div className={`hero-copy`}>
+              <h1 className={`hero-title`}>
+                {data.allContentfulHomePageAssembly.nodes[0].heroTitleEnglish}
+              </h1>
+              <p className={`hero-description`}>
+                {
+                  data.allContentfulHomePageAssembly.nodes[0]
+                    .heroDescriptionEnglish.heroDescriptionEnglish
+                }
+              </p>
+              <div>
+                <a href='tel:03180225010' className={`cta-btn`}>
+                  Call Us <PhoneSVG className={`cta-phone-icon`} />
+                </a>
+              </div>
+            </div>
+          </Col>
+          <Col lg={8} className={`order-first order-lg-last`}>
+            <HeroSVG className={`hero-svg-width`} />
+          </Col>
+        </Row>
+      </section>
+    )}
+  />
 );
 
 export default HeroSection;
