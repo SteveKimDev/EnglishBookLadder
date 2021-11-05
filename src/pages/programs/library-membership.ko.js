02ac1de254111769a261b28bd5908c3a7579452c
stevekim72 <steve.kim72@yahoo.com>
2021-11-05 14:11:41 -0700

    Library membership page - hero korean

---
+++ src/pages/programs/library-membership.ko.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from '../../components/layout/layout-ko';
+import HeroSection from '../../components/library-membership-page/01-hero-ko';
+
+const LibraryMembershipPage = () => (
+  <div id='library-page'>
+    <Layout>
+      <HeroSection />
+    </Layout>
+  </div>
+);
+
+export default LibraryMembershipPage;
